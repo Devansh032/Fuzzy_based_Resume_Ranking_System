7857460f42cb7131f9978bdff6c7a88ad97da08e
Devansh032 <devanshsinghal627@gmail.com>
2025-02-27 18:13:05 +0530

    append file - 1. analyse resume using llm and process resume

---
+++ Ats_Checker/core/resumechecker/analyzer.py
@@ -0,0 +1,63 @@
+import fitz
+import os   
+import re
+from groq import Groq
+import json
+
+def extract_text_from_pdf(pdf_path):
+    text = ""
+    with fitz.open(pdf_path) as pdf:
+        for page in pdf:
+            text += page.get_text()
+    return text.strip()
+
+API_KEY = "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"
+
+def analyse_resume_with_llm(resume_text:str,job_description:str) -> dict :
+    prompt = f"""
+        You are an AI assistant that analyses resumes for a software engineering job application.
+        Given a resume and a job description, extract the following details:
+        1. Identify all skills mentioned in the resume along with softskills and courses studied in the college.
+        2. Calculate the total years of expeiernce.
+        3. Categories the projects based on the domain (e.g, AI,Webdevelopment,Cloud etc)
+        4. Rank the resume relevance to the job description on a scale of 0 to 100.
+
+        Resume:
+        {resume_text}
+
+        Job Description:
+        {job_description}
+
+        Provide the output in valid JSON format with this strucure :
+        {{
+            "rank" : "<percentage>",
+            "skills": ["skill1", "skill2", "skill3"],
+            "total_experience": "<number of years",
+            "project_category": ["category1","category2",....]
+        }}
+
+    """ 
+
+    try:
+        client = Groq(api_key=API_KEY)
+        response = client.chat.completions.create(
+            model= "llama-3.3-70b-versatile",
+            messages = [{'role' : 'user','content' : prompt}],
+            temperature=0.7,
+            response_format={'type': "json_object"}
+        )
+        result = response.choices[0].message.content
+        return json.loads(result)
+    
+    except Exception as e:
+        print(e)
+
+
+def process_resume(pdf_path,job_description) :
+    try : 
+        resume_text = extract_text_from_pdf(pdf_path)
+        data = analyse_resume_with_llm(resume_text,job_description)
+        return data
+    except Exception as e:
+        print(e)
+        return None
