eebb757c1502aebe0c584d48c40f0b8c5d0a0cf7
Devansh032 <devanshsinghal627@gmail.com>
2025-03-24 09:56:58 +0530

    backend fuzzy logic completed

--- Ats_Checker/core/resumechecker/views.py
+++ Ats_Checker/core/resumechecker/views.py
@@ -5,8 +5,8 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializer import JobDesriptionSerializer,JobDescription,ResumeSerializer,Resume
-from .analyzer import process_resume
-
+from .analyzer import process_resume,process_job_description
+from scripts import process_candidate,calculate_skill_score,calculate_project_relevance,fuzzy_candidate_Scoring
 
 class JobDescriptionAPI(APIView):
     def get(self, request):
@@ -21,6 +21,7 @@ class JobDescriptionAPI(APIView):
 
 class AnalyzeResumeAPI(APIView) :
     def post(self,request) :
+        print("start")
         try :
             data = request.data
             if not data.get('job_description') :
@@ -29,7 +30,6 @@ class AnalyzeResumeAPI(APIView) :
                     'message':'job_description is required',
                     'data' :{}
                 })
-            
             serializer = ResumeSerializer(data=data)
             if not serializer.is_valid() :
                 return Response({
@@ -40,19 +40,30 @@ class AnalyzeResumeAPI(APIView) :
             
             serializer.save()
             _data = serializer.data
+            job_id = data.get("job_description")
             resume_instance = Resume.objects.get(id=_data['id'])
             resume_path = resume_instance.resume.path
-            data = process_resume(resume_path,JobDescription.objects.get(id=data.get('job_description')).job_description)
+            data = process_resume(resume_path,JobDescription.objects.get(id=job_id).job_description)
+            job_data = process_job_description(JobDescription.objects.get(id=job_id).job_description)
+            
+            job = JobDescription.objects.filter(id=job_id).first()
+            print("start")
+            rank = process_candidate(dict(data),str(job),dict(job_data.get("skills")),calculate_skill_score,calculate_project_relevance,fuzzy_candidate_Scoring)
+            print("exit")
+            print(rank)
+            data["rank"] = rank
+
             return Response({
                 'status' : True,
-                'message':'resume analysed',
-                'data' : data
+                'message': str(job),
+                'data' : data,
+                'job_data':job_data
             })
         
         except Exception as e:  
             return Response({
                 'status' : False,
-                'message':str(e),
+                'message': str(e),
                 'data' :{}
             })
         
